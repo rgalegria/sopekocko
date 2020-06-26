8a8994d469737ab75ddf467736b6d7e47dbdd5a7
rgalegria <59605825+rgalegria@users.noreply.github.com>
2020-06-26 19:20:20 +0200

    Signup/Login, controllers, routes et middleware implementés

---
+++ Backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    "mongodb+srv://rgalegria:OCproject6@cluster0-zcn6k.azure.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connection to MongoDB succeeded !"))
+  .catch(() => console.log("Connection to MongoDB failed !"));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
+
+module.exports = app;
